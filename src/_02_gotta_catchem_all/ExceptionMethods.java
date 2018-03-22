15a010f147cdd45530ee0e121892dd8cb66d2066
Lucas-Luwa <League@AdministorsiMac.attlocal.net>
2018-03-21 17:59:14 -0700

    Done
    Completed all

--- src/_02_gotta_catchem_all/ExceptionMethods.java
+++ src/_02_gotta_catchem_all/ExceptionMethods.java
@@ -3,21 +3,35 @@ package _02_gotta_catchem_all;
 import java.util.Stack;
 
 public class ExceptionMethods {
-	public String reverseString(String a) throws IllegalArgumentException {
-		if (a.equals("")) {
+
+	public double divide(double a, double b) throws IllegalArgumentException {
+		
+		if (b == 0.0) {
 			throw new IllegalArgumentException();
+		}
+		double c = a / b;
+		return c;
+	}
+
+	public String reverseString(String a) throws IllegalStateException {
+		if (a.equals("")) {
+			throw new IllegalStateException();
 		}
 		String reverse = "";
 		Stack<Character> character = new Stack<Character>();
-		for (int i = 0; i < a.length(); i++) {
+		int c = a.length();
+		for (int i = 0; i <c; i++) {
 
-			character.add(a.charAt(i));
+			character.push(a.charAt(i));
 
 		}
-		for (int i = 0; i < a.length(); i++) {
-
+		for (int i = 0; i < c; i++) {
+			System.out.println(reverse);
 			reverse += character.pop();
+		
+			
 		}
+	
 		return reverse;
 	}
 
